fdc6fc026c427cc6a3ac55d1f3ca35fb3e6de1e2
Hewitt02 <Hewitt1203@gmail.com>
2023-10-29 09:13:04 -0400

    Will

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+const quizQuestions = [
+    {
+        question: "Which Pokemon is a water type?",
+        choices: ["Charmander", "Bulbasaur", "Squirtle", "Pikachu"],
+        answers: "Squirtle",
+    },
+    {
+        question: "Which of the following Pokemon can evolve?",
+        choices: ["Blastoise", "Mewtwo", "Groudon", "Bayleef"],
+        answers: "Bayleef",
+
+    },
+    {
+        question: "How many pokemon can you carry with you at one time?",
+        choices: ["5", "1", "10", "6",],
+        answers: "6",
+
+    },
+    {
+        question: "Which of the following pokemon have more than one type?",
+        choices: ["Sentret", "Nosepass", "Sableye", "Magikarp"],
+        answers: "Sableye",
+
+    },
+    {
+        question: "Which of the following Pokemon is considered a Legendary Pokemon?",
+        choices: ["Mewtwo", "Mew", "Meloetta", "Jirachi"],
+        answers: "Mewtwo",
+
+    },
+    
+
+]
+
